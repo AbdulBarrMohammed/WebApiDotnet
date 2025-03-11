11746527301ce064f6b7fb033c8862e3d5af8096
Barr Mohammed <barrmohammed.tpa@gmail.com>
2025-03-10 21:04:01 -0400

    accountdb

--- WebApplication1/Data/AccountDbContext.cs
+++ WebApplication1/Data/AccountDbContext.cs
@@ -11,8 +11,19 @@ namespace WebApplication1.Data
         public List<Account> Accounts { get; set; } = new List<Account>();
         private AccountDbContext() {
             Accounts = new List<Account>() {
-                new Account { AccountType = AccountType.Main, Id = 1, Name = "Acc1", Number = "AC1"},
-                new Account { AccountType = AccountType.Sub, Id = 2, Name = "Acc2", Number = "AC2"},
+                new Account { AccountType = AccountType.Main, Id = 1, Name = "Acc1", Number = "AC1",
+                Cards = new List<Card>() {
+                    new Card{ Id = 1, Number = "3434-4455-3444", ExpireDate = "06/28", HolderName = "John"},
+                    new Card{ Id = 2, Number = "3433-4355-3444", ExpireDate = "06/24", HolderName = "James"}
+                }
+
+                },
+                new Account { AccountType = AccountType.Sub, Id = 2, Name = "Acc2", Number = "AC2",
+                Cards = new List<Card>() {
+                    new Card{ Id = 4, Number = "3434-4455-3444", ExpireDate = "06/28", HolderName = "Dave"},
+                    new Card{ Id = 5, Number = "3433-4355-3444", ExpireDate = "06/24", HolderName = "Dan"}
+                }
+                },
 
             };
         }
--- WebApplication1/Models/Account.cs
+++ WebApplication1/Models/Account.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,8 @@ namespace WebApplication1.Models
         public string Name { get; set; }
         public string Number { get; set; }
         public AccountType AccountType { get; set; }
+
+        public ICollection<Card> Cards {get; set;} = new List<Card>();
 
 
     }
